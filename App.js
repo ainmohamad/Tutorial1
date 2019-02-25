d82cdb4f43925ef27bddd85e285f484f268d521e
ainmohamad <nurainmohd97@gmail.com>
2019-02-25 22:28:29 +0800

    first commit

---
+++ App.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+
+export default class App extends React.Component {
+  render() {
+    return (
+      <View style={styles.container}>
+        <Text>My name is Nur ain mohamad</Text>
+        <Text>Matric number: 1620510</Text>
+      </View>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
